0d56dc53ebf8ff62e75d33bc70a5bb7e7a0c322e
InAe-Lee <106795795+inae0615@users.noreply.github.com>
2022-07-20 16:22:20 +0900

    Update grade_system.py

--- grade_system.py
+++ grade_system.py
@@ -1,39 +1,35 @@
+import math
 
-coffee=3
-bubble=2
+def main():
+    midterm = int(input("Enter grade on midterm: "))
+    final = int(input("Enter grade on final exam: "))
+    
+    average = (midterm + final + final)/3
+    average = math.ceil(average)
+    
+    grade=semesterGrade(average)
+    print("Semeter Grade:",grade)
 
-while True:
-    print("*"*20)
-    print(" Welcome to \n LIA's cafe")
-    print("*"*20)
-    print("MENU\n1. coffee \n2. bubble tea")
-    print("*"*20)
-#1
-    seclection=eval(input("Make a selection from the menua(You Should input number 1 or 2): "))
-    if seclection==1:
-        quantity=int(input("You chose coffee \nGive quantity of coffee: "))
-        if seclection==1 and coffee>=quantity: 
-            print("Here is what you orderd Thank you!")
-            coffee-=quantity
-        else:
-            print("There is",coffee,"cups left")
-#2        
-    elif seclection==2:
-        quantity=int(input("You chose bubble tea \nGive quantity of bubble tea: "))
-        if seclection==2 and bubble>=quantity: 
-            print("Here is what you orderd. Thank you!")
-            bubble-=quantity
-        else:
-            print("There is",bubble,"cups left")   
-#3   
-    else:
-        print("Not on the menu")
+def semesterGrade(average):
 
-#4    
-    if coffee==0 and bubble==0:
-            print("="*20)
-            print(" All sold out \n LIA's cafe closed")
-            break
+    if average>= 90 :
+        grade = 'A'
         
+    elif average>= 80 :
+        grade = 'B'
+        
+    elif average>= 70:
+        grade = 'C'
+        
+    elif average>= 60 :
+        grade = 'D'
+        
+    else:
+        grade = 'F'   
 
+    return grade
+
+main()
+
+ 
     
